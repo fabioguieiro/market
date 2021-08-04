e735fb3d19fb4d7ca9315abfa0c2a6414a08e266
fabioguieiro <fabioguieiro@gmail.com>
2021-08-04 20:30:43 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from 'axios'
+import { useEffect, useState } from "react";
 
 function App() {
+  useEffect(()=>{
+    axios.get('https://market-b9812-default-rtdb.firebaseio.com/products.json').then(response =>{
+      console.log('products: ', response.data)
+      setProductData(response.data)
+      console.log('productdata: ', productData)
+    })
+  }, [])
+  const [productData, setProductData] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <table style={{width: '100%'}} >
+        <tr>
+          <th>Item Name</th>
+          <th>Preço</th>
+        </tr>
+        <tr style={{height: 30}}>
+          <td>Cornetto Soft Erdbeer Ktn</td>
+          <td>0.53€/PCS</td>
+        </tr>
+        <tr style={{height: 30}}>
+          <td>Broccoli</td>
+          <td>0.53€/PCS</td>
+        </tr>
+      </table> */}
     </div>
   );
 }
