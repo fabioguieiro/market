e19101a669162393d6499a565fd94ef3626bf862
fabioguieiro <fabioguieiro@gmail.com>
2021-08-07 12:38:00 -0300

    feat: all functionalities added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
 import "./App.css";
-import axios from 'axios'
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  useEffect(()=>{
-    axios.get('https://market-b9812-default-rtdb.firebaseio.com/products.json').then(response =>{
-      console.log('products: ', response.data)
-      setProductData(response.data)
-      console.log('productdata: ', productData)
-    })
-  }, [])
+  useEffect(() => {
+    axios
+      .get("https://market-b9812-default-rtdb.firebaseio.com/products.json")
+      .then((response) => {
+        console.log("products: ", response.data);
+        setProductData(response.data);
+        console.log("productdata: ", productData);
+      });
+  }, []);
   const [productData, setProductData] = useState([]);
+  const [orderPrice, setOrderPrice] = useState(0);
+  const SHIPPING = 30.89;
+  const handleCheckbox = (itemPrice, value) =>{
+    const sum = orderPrice+itemPrice;
+    const sub = orderPrice-itemPrice;
+    value.target.checked ? setOrderPrice(sum) : setOrderPrice(sub);
+  }
   return (
     <div className="App">
-      {/* <table style={{width: '100%'}} >
+      <table style={{ width: "100%" }}>
         <tr>
+          <th>Item Name</th>
           <th>Item Name</th>
           <th>Preço</th>
         </tr>
-        <tr style={{height: 30}}>
-          <td>Cornetto Soft Erdbeer Ktn</td>
-          <td>0.53€/PCS</td>
-        </tr>
-        <tr style={{height: 30}}>
-          <td>Broccoli</td>
-          <td>0.53€/PCS</td>
-        </tr>
-      </table> */}
+        {productData
+          ? productData.map((item, i) => {
+              return (
+                <tr key={i} style={{ height: 30 }}>
+                  <td>
+                    <input type='checkbox' onChange={e => handleCheckbox(item.price, e)} ></input>
+                  </td>
+                  <td><img style={{ width: 20 }} src={item.img}></img>{item.name}</td>
+                  <td>
+                    {item.price}/{item.unity}
+                  </td>
+                </tr>
+              );
+            })
+          : null}
+      </table>
+      <div style={{backgroundColor: '#ccc'}}>
+            <p>Order Price: R$ {orderPrice.toFixed(2)}</p>
+            <p>Shipping: R$ {SHIPPING}</p>
+            <p>Total: R$ {(orderPrice + SHIPPING).toFixed(2)}</p>
+      </div>
     </div>
   );
 }
